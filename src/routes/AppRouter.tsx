cb3e7caa14b6f62bc3badd1c507a70c9dca29fed
jocollier <jocollier.it@gmail.com>
2025-08-06 07:39:58 +0100

    feat: Add animated blob background with layout component

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import BlobLayout from "../layouts/BlobLayout/BlobLayout";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>Main Layout Placeholder</div>}>
+      <Route path="/" element={<BlobLayout />}>
         <Route index element={<Navigate to="/credentials" replace />} />
         <Route path="credentials" element={<div>Credentials Page Placeholder</div>} />
         <Route path="forgot-password" element={<div>Forgot Password Page Placeholder</div>} />
